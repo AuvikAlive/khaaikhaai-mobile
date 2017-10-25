6a25db8ac6666f064d4676311bc61c870641cab5
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2017-10-25 23:37:05 +0600

    added firebase

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import Tabs from './src/components/tabs/Tabs'
-import store from './src/store'
+import { store } from './src/store'
 
 const App = () => (
   <Provider store={store}>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import createSagaMiddleware from 'redux-saga'
+import * as firebase from 'firebase'
+import config from '../firebaseConfig'
+
+firebase.initializeApp(config)
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
 
-export default store
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+export const database = firebase.database()
