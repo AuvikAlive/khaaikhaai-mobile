de7a938453472387fc39a3bce99ff865b103009a
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2017-10-25 22:13:18 +0600

    refactored redux store creation

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './src/reducers/rootReducer'
 import Tabs from './src/components/tabs/Tabs'
-
-let store = createStore(rootReducer)
+import store from './src/store'
 
 const App = () => (
   <Provider store={store}>
