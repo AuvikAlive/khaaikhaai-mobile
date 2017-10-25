2bcb1d481d9783494308fb3cb600383dea7aebab
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2017-10-25 22:10:24 +0600

    added eslint

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './src/reducers/rootReducer'
-
-import Restaurants from './src/components/restaurants/Restaurants'
 import Tabs from './src/components/tabs/Tabs'
 
 let store = createStore(rootReducer)
