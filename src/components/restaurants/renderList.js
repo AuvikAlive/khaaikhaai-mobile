323fa0b44091bfefdc6a01a56dbab60a94f7aae0
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2017-11-08 04:14:12 +0600

    added throttle to Restaurants inifity request

--- src/components/restaurants/renderList.js
+++ src/components/restaurants/renderList.js
@@ -1,11 +1,12 @@
 // @flow
 
 import React from 'react'
-import { View, ListView } from 'react-native'
+import { ListView } from 'react-native'
 import { List } from 'react-native-elements'
 import SearchRestaurants from '../searchRestaurants/SearchRestaurantsContainer'
 import renderItem from './renderItem'
 import Indicator from '../indicator/Indicator'
+import throttle from 'lodash/throttle'
 import type { listItem } from './listItemType'
 
 const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
@@ -18,9 +19,10 @@ const renderList = (
   loading
 ) => {
   const dataSource = ds.cloneWithRows(list)
-  const fetchNextRestaurants = () => {
+  const fetchNextRestaurants = throttle(() => {
     fetchRestaurants(20, lastItem.restaurantID)
-  }
+  }, 2000)
+
   return (
     <List containerStyle={{ marginTop: 25, marginBottom: 60 }}>
       <SearchRestaurants />
@@ -29,7 +31,7 @@ const renderList = (
         renderRow={item => renderItem(item, navigate)}
         keyExtractor={(item: listItem): string => item.restaurantName}
         onEndReached={fetchNextRestaurants}
-        onEndReachedThreshold={5}
+        onEndReachedThreshold={60}
         renderFooter={() => (loading ? <Indicator /> : null)}
       />
     </List>
